c9880d2bcf09a319c1fe23cc1fd5b0a63b8cc889
Kichan Lee <kichlee@cx1r5s7.42seoul.kr>
2024-04-14 14:50:53 +0900

    complete

--- rm.sh
+++
@@ -1,7 +0,0 @@
-#! bin/sh
-
-rm -rf /Users/kichan/Desktop/Inception/srcs/data/db/*
-rm -rf /Users/kichan/Desktop/Inception/srcs/data/web/*
-
-docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi -f $(docker images -qa); docker volume rm $(docker
-volume ls -q); docker network rm $(docker network ls -q) 2>/dev/null
